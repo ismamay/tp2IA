e860489d5ba009f539213fe8c1338d989b76bf03
ismamay <ismamayy@yahoo.fr>
2026-05-06 00:58:31 -0400

    Geolocalisation terminé

--- src/main/java/ht/ueh/mbds/Ismael_Romelus/AssistantMeteo.java
+++ src/main/java/ht/ueh/mbds/Ismael_Romelus/AssistantMeteo.java
@@ -1,5 +1,9 @@
 package ht.ueh.mbds.Ismael_Romelus;
 
+import dev.langchain4j.service.SystemMessage;
+
 public interface AssistantMeteo {
+    @SystemMessage("Tu es un assistant polyvalent. Tu peux répondre à toutes les questions. " +
+            "Tu as accès à des outils pour obtenir les prévisions météo et les coordonnées des villes.")
     String chat(String userMessage);
 }
